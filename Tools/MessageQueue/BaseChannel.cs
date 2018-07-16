6659b65a4e6cdd2b10963b34661e7e0f96383c20
Mefodey <mefodei.link@gmail.com>
2018-07-17 01:15:40 +0300

    - fix game errors

--- Tools/MessageQueue/BaseChannel.cs
+++ Tools/MessageQueue/BaseChannel.cs
@@ -1,123 +1,117 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
+using Assets.Scripts.Extensions;
 using Assets.Scripts.MessageQueue;
-using Assets.Scripts.ProfilerTools;
 using UniRx;
-using UnityEngine;
 
-public class BaseChannel : IChannel
-{
-    protected Subject<IMessage> _input;
-    protected Subject<IMessage> _output;
-    protected Subject<Type> _typeAttached;
-    protected Subject<Type> _typeRemoved;
+namespace Assets.Scripts.MessageQueue {
 
-    protected List<IDisposable> _disposables;
-    protected HashSet<Type> _registeredTypes;
+    public class BaseChannel : IChannel {
 
-    public BaseChannel() {
+        protected Subject<IMessage> _input;
+        protected Subject<IMessage> _output;
+        protected Subject<Type> _typeAttached;
+        protected Subject<Type> _typeRemoved;
 
-        _input = new Subject<IMessage>();
-        _output = new Subject<IMessage>();
-        _disposables = new List<IDisposable>();
-        _registeredTypes = new HashSet<Type>();
+        private List<IDisposable> _disposables;
+        private Dictionary<Type, object> _registeredTypes;
 
-    }
+        private IDisposable _inputDisposable;
+        private IDisposable _outputDisposable;
 
-    public bool IsDisposed { get; protected set; }
+        public BaseChannel(IMessageSystem<IMessage> messageSystem) {
 
-    public IObservable<IMessage> Input => _input;
+            _input = new Subject<IMessage>();
+            _output = new Subject<IMessage>();
+            _disposables = new List<IDisposable>();
+            _registeredTypes = new Dictionary<Type, object>();
 
-    public IObservable<IMessage> Output => _output;
-
-    public IObservable<Type> OnAttach => _typeAttached;
-
-    public IObservable<Type> OnUnsubscribe => _typeRemoved;
-
-    public void Send(IMessage message) {
-
-        ProcessMessage(message, _output, ValidateOutputMessage);
-
-    }
-
-    public IEnumerable<Type> GetRegisteredTypes() {
-        return _registeredTypes;
-    }
-
-    public bool IsRegistered(Type type) {
-        return _registeredTypes.Contains(type);
-    }
-
-    public IDisposable Attach<T>(Action<T> messageAction, Func<T, bool> filter = null)
-        where T : class {
-
-        var targetType = typeof(T);
-        var disposable = _input.Where(x => Filter<T>(x.Context, filter)).
-            Subscribe(x => messageAction(x.Context as T)).AddTo(_disposables);
-        _registeredTypes.Add(targetType);
-
-        return disposable;
-
-    }
-
-    public void SendToInput(IMessage message) {
-
-        ProcessMessage(message, _input, ValidateInputMessage);
-
-    }
-    
-    public void Dispose()
-    {
-        _input.OnCompleted();
-        _output.OnCompleted();
-
-        _input.Dispose();
-        _output.Dispose();
-
-        foreach (var disposable in _disposables) {
-            disposable.Dispose();
+            _outputDisposable = messageSystem.AddPublicher(Output);
         }
 
-        _disposables.Clear();
+        public bool IsDisposed { get; protected set; }
 
-        IsDisposed = true;
+        public IObserver<IMessage> Input => _input;
+
+        public IObservable<IMessage> Output => _output;
+
+        public IObservable<Type> OnAttach => _typeAttached;
+
+        public IObservable<Type> OnUnsubscribe => _typeRemoved;
+
+        public void Send(IMessage message) {
+
+            ProcessMessage(message, _output, ValidateOutputMessage);
+
+        }
+
+        public bool IsRegistered(Type type) {
+            return _registeredTypes.ContainsKey(type);
+        }
+
+        public IObservable<T> Attach<T>()
+            where T : class {
+
+            var targetType = typeof(T);
+
+            if (_registeredTypes.TryGetValue(targetType, out object outObservable) == false) {
+                outObservable = _input.Select(x => x.Context as T).Where(x => x != null);
+                _registeredTypes.Add(targetType, outObservable);
+            }
+
+            return outObservable as IObservable<T>;
+        }
+
+        public void SendToInput(IMessage message) {
+
+            ProcessMessage(message, _input, ValidateInputMessage);
+
+        }
+
+        public void Dispose() {
+            _input.OnCompleted();
+            _output.OnCompleted();
+
+            _input.Cancel();
+            _output.Cancel();
+            _outputDisposable.Cancel();
+            _outputDisposable = null;
+
+            if (_inputDisposable != null)
+                _inputDisposable.Cancel();
+            _inputDisposable = null;
+
+            _disposables.Cancel();
+            _disposables.Clear();
+
+            IsDisposed = true;
+        }
+
+        #region private methods
+
+        protected virtual bool ValidateOutputMessage(IMessage message) {
+
+            return true;
+
+        }
+
+        protected virtual bool ValidateInputMessage(IMessage data) {
+
+            return true;
+
+        }
+
+
+        private void ProcessMessage(IMessage messageObject, IObserver<IMessage> observable,
+            Func<IMessage, bool> validator) {
+            if (messageObject == null) return;
+
+            var validateResult = validator(messageObject);
+            if (validateResult == false) return;
+            observable.OnNext(messageObject);
+
+        }
+
+        #endregion
     }
-
-    #region private methods
-
-    
-    private bool Filter<T>(object context, Func<T, bool> filter = null) where T: class {
-
-        var data = context as T;
-        if (data == null) return false;
-        var result = filter?.Invoke(data) ?? true;
-        return result;
-
-    }
-
-    protected virtual bool ValidateOutputMessage(IMessage message) {
-
-        return true;
-
-    }
-
-    protected virtual bool ValidateInputMessage(IMessage data) {
-
-        return true;
-
-    }
-
-
-    private void ProcessMessage(IMessage messageObject,IObserver<IMessage> observable, Func<IMessage,bool> validator)
-    {
-        if (messageObject == null) return;
-
-        var validateResult = validator(messageObject);
-        if (validateResult == false) return;
-        observable.OnNext(messageObject);
-
-    }
-
-    #endregion
 }
--- Tools/MessageQueue/MessageSystem.cs
+++ Tools/MessageQueue/MessageSystem.cs
@@ -1,94 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using Assets.Tools.Utils;
 using Assets.Scripts.ProfilerTools;
-using UniRx;
 using UnityEngine;
 
 namespace Assets.Scripts.MessageQueue
 {
-    public class MessageSystem : IMessageSystem
+    public class MessageSystem : ObserableRxHub<IMessage>
     {
 
         private static Color _messageLogColor = new Color(0, 100, 0);
 
-
-        #region private properties
-
-        /// <summary>
-        /// added channels
-        /// </summary>
-        private readonly Dictionary<IChannel, IDisposable> _channels = new Dictionary<IChannel, IDisposable>();
-        private readonly List<IChannel> _outputChannels = new List<IChannel>();
-        private readonly Dictionary<IChannel,int> _outputChannelsMap = new Dictionary<IChannel, int>();
-
-        #endregion
-
-        #region constructor
-
-        public MessageSystem() { }
-
-        #endregion
-
-        public void Register(IChannel channel) {
-            int index;
-            if (_outputChannelsMap.TryGetValue(channel, out index) == false) {
-                index = _outputChannels.Count;
-                _outputChannels.Add(channel);
-                _outputChannelsMap[channel] = index;
-            }
-        }
-
-        public void RemoveRegistration(IChannel channel)
-        {
-            Unregister(channel);
-        }
-
-        public void AddSource(IChannel channel)
-        {
-            if (_channels.ContainsKey(channel))
-            {
-                GameLog.LogError("Channel already registered");
-                return;
-            }
-            var disposable = channel.Output.Subscribe(x => OnMessageReceived(x, channel));
-            _channels[channel] = disposable;
-        }
-
-        public void RemoveSource(IChannel channel)
-        {
-            if (!_channels.ContainsKey(channel))
-                return;
-            _channels[channel].Dispose();
-            _channels.Remove(channel);
-        }
-
         #region private methods
 
-        private void OnMessageReceived(IMessage message, IChannel sender)
+        protected override void OnMessageReceived(IMessage message, IObservable<IMessage> sender)
         {
             LogMessage(message,true);
 
-            if (message == null || message.Type == null)
-            {
+            if (message == null || message.Type == null) {
                 return;
             }
 
-            foreach (var channel in _outputChannels) {
+            foreach (var output in _subscribers) {
 
-                if (channel.IsDisposed)
-                {
-                    Unregister(channel);
-                    continue;
-                }
+                var observer = output.Key;
+                if (observer == sender) continue;
 
-                if (channel == sender)continue;
-                
-                if (!channel.IsRegistered(message.Type))continue;
-
-                channel.Send(message);
+                observer.OnNext(message);
             }
+
+
             //relese message Data
             message.Despawn();
         }
@@ -107,25 +48,6 @@ namespace Assets.Scripts.MessageQueue
                 GameLog.LogFormat("[MSQ Received] Type:{0} Content: {1} Sender {2}",
                     _messageLogColor, type, message.Context, message.Sender);
             }
-        }
-
-        private void Unregister(IChannel channel) {
-
-            int index;
-            if (_outputChannelsMap.TryGetValue(channel, out index)) {
-                _outputChannels.Remove(channel);
-                _outputChannelsMap.Remove(channel);
-            }
-
-        }
-
-        public void Dispose()
-        {
-            foreach (var disposable in _channels) {
-                disposable.Value.Dispose();
-            }
-            _channels.Clear();
-            _outputChannels.Clear();
         }
 
         #endregion
