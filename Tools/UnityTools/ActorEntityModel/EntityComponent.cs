75916c7c7c931a0816607b5c18903e7d43c64b27
Mefodey <mefodei.link@gmail.com>
2018-10-02 04:45:09 +0300

    - add entitycomponent

---
+++ Tools/UnityTools/ActorEntityModel/EntityComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tools.ActorModel;
+using UnityEngine;
+
+namespace Tools.ActorModel {
+
+	public class EntityComponent : MonoBehaviour {
+
+		private EntityObject _entity = new EntityObject();
+
+		public IEntity Entity => _entity;
+
+	}
+}
