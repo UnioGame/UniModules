d927d854f81999e33b33a3b74794d8bac5cf84d7
Mefodey <mefodei.link@gmail.com>
2018-10-05 01:51:38 +0300

    add ActorComponent

---
+++ Tools/UnityTools/ActorEntityModel/ActorComponent.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Tools.UnityTools.Common;
+using Assets.Tools.UnityTools.Interfaces;
+using Assets.Tools.UnityTools.StateMachine.Interfaces;
+using Assets.Tools.UnityTools.StateMachine.UniStateMachine;
+using Assets.Tools.UnityTools.UniRoutine;
+using UnityEngine;
+
+public class ActorComponent : MonoBehaviour, IDisposable {
+		
+	private IContext _context = new ContextData();
+	private IDisposableItem _disposableItem;
+	
+	[SerializeField]
+	private UniStateComponent _state;
+	[SerializeField]
+	private bool _launchOnStart = true;
+
+	public IContextStateBehaviour<IEnumerator> State => _state;
+
+	public IContext Context => _context;
+	
+	// Use this for initialization
+	private void Start () {
+
+		if (_launchOnStart == false)
+			return;
+
+		Launch();
+	}
+
+	public void Dispose() {
+		Context?.Release();
+		_disposableItem?.Dispose();
+	}
+	
+	public void Launch() {
+		
+		AddContextData();
+		if (State == null)
+			return;
+		
+		var awaiter = _state.Execute(Context);
+		_disposableItem = awaiter.RunWithSubRoutines();
+		
+	}
+
+	protected virtual void AddContextData() {
+		
+	}
+
+	private void OnDestroy() {
+		
+		Dispose();
+		
+	}
+
+}
