30cbaa93fa73f3d73bdf4f374d474127a0986b3a
Mihaylov Sergey <sergey.mihaylov@zimad.com>
2019-01-24 18:39:20 +0300

    - update Effect api - update Car Vehicle model effector triggers

---
+++ UnityTools/Extension/DisposableExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Modules.UniTools.UnityTools.Extension
+{
+    public static class DisposableExtension
+    {
+        public static void DisposeItems<TTarget>(this List<TTarget> disposables)
+            where  TTarget : IDisposable
+        {
+            for (int i = 0; i < disposables.Count; i++)
+            {
+                var item = disposables[i];
+                item.Dispose();
+            }
+            disposables.Clear();
+        }
+    }
+}
