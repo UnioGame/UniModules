9783ce2388d36752691b28c43a19afe834ba8faa
Mefodei <mefodei.link@gmail.com>
2019-02-12 14:02:09 +0300

    - move command terminal asset to module ThirdParty folder

---
+++ Thirdparty/CommandTerminal/RegisterCommandAttribute.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Reflection;
+
+namespace CommandTerminal
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class RegisterCommandAttribute : Attribute
+    {
+        public int MinArgCount { get; set; } = 0;
+
+        public int MaxArgCount { get; set; } = -1;
+
+        public string Name { get; set; }
+        public string Help { get; set; }
+
+        public CommandPermissionLevel PermissionLevel { get; set; } = CommandPermissionLevel.Any;
+
+        public RegisterCommandAttribute(string command_name = null) {
+            Name = command_name;
+        }
+
+        public RegisterCommandAttribute(string command_name, CommandPermissionLevel level) {
+            Name = command_name;
+            PermissionLevel = level;
+        }
+    }
+}
---
+++ Thirdparty/CommandTerminal/TerminalClient.cs
@@ -0,0 +1,37 @@
+﻿using CommandTerminal;
+using UnityEngine;
+
+namespace _GetOverIt.Content._Programmers.Scripts.TerminalCommands
+{
+	public class TerminalClient : MonoBehaviour
+	{
+		private float _lastActivationTime;
+		private float _touchPressedTime;
+		
+		private float _touchDuration = 4f;
+		[SerializeField]
+		private Terminal _terminal;
+		[SerializeField]
+		private int _touchCount = 5;
+
+		// Update is called once per frame
+		private void Update () 
+		{
+			if (Input.touchCount == _touchCount)
+			{
+				_touchPressedTime += Time.deltaTime;
+
+				if (_touchPressedTime < _touchDuration)
+					return;
+
+				_touchPressedTime = 0;
+				
+				_terminal.ToggleState(TerminalState.OpenSmall);
+			}
+			else
+			{
+				_touchPressedTime = 0f;
+			}
+		}
+	}
+}
