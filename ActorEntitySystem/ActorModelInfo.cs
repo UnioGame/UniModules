7f14e0eccc4503e13d0ead4bab1ba1b4c53fc3bf
Mefodei <mefodeilink@gmail.com>
2019-03-27 02:45:48 +0300

    - update goi modules

--- ActorEntitySystem/ActorModelInfo.cs
+++ ActorEntitySystem/ActorModelInfo.cs
@@ -1,9 +1,10 @@
-﻿using UniModule.UnityTools.ActorEntityModel;
-using UniModule.UnityTools.UniPool.Scripts;
-using UnityEngine;
-
+﻿
 namespace Modules.UniTools.ActorEntitySystem
 {
+    using UniModule.UnityTools.ActorEntityModel;
+    using UniModule.UnityTools.UniPool.Scripts;
+    using UnityEngine;
+
     public class ActorModelInfo<TModel> : ActorInfo
         where TModel : ActorModel<TModel>,new()
     {
