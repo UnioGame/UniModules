7cc4ee639ce8647a4b3aba459bfe545be07ae755
MikhailovSergey <mikhailovss@plavalaguna.ru>
2019-04-01 16:33:02 +0300

    - project refactorings

---
+++ UniNodeSystem/Attributes/TypeFilterAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Modules.UniTools.UniNodeSystem.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
+    public class TypeFilterAttribute : Attribute
+    {
+
+        public Type[] Types;
+        
+        public TypeFilterAttribute(params Type[] types)
+        {
+            Types = types;
+        }
+        
+    }
+}
