36f5c83010c46cc38700ce8e01119fb3f1f9e51c
Mefodei <mefodeilink@gmail.com>
2019-05-22 00:52:49 +0300

    - update unibuild pipeline package

---
+++ UnityBuild/Editor/ClientBuild/Commands/PreBuildCommands/UpdateVersionCommand/UpdateVersionCommand.cs
@@ -0,0 +1,61 @@
+﻿namespace UniGreenModules.UnityBuild.Editor.ClientBuild.Commands.PreBuildCommands 
+{
+    using System.Text;
+    using Interfaces;
+    using UnityEditor;
+    using UnityEngine;
+
+    /// <summary>
+    /// update current project version
+    /// </summary>
+    [CreateAssetMenu(menuName = "UnityBuild/PreBuildCommands/Update Project Version", fileName = "UpdateVersionCommand")]
+    public class UpdateVersionCommand : UnityPreBuildCommand
+    {
+        [SerializeField]
+        private int minBuildNumber = 1;
+        
+        public override void Execute(IUniBuilderConfiguration configuration)
+        {
+
+            var buildParameters = configuration.BuildParameters;
+            UpdateBuildVersion(buildParameters.BuildTarget, buildParameters.BuildNumber);
+            
+        }
+        
+        public void UpdateBuildVersion(BuildTarget buildTarget,int buildNumber)
+        {
+            var buildVersionHandler = new BuildVersionProvider();
+            var logBuilder = new StringBuilder(200);
+
+            var activeBuildNumber = buildNumber + minBuildNumber;
+            var bundleVersionCode = buildVersionHandler.GetActiveBuildNumber(buildTarget,activeBuildNumber);
+            var buildNumberString =  bundleVersionCode.ToString();
+            var bundleVersion = PlayerSettings.bundleVersion;
+            
+            var version = buildVersionHandler.GetBuildVersion(buildTarget,bundleVersion,buildNumber);
+            
+            PlayerSettings.bundleVersion = version;
+            PlayerSettings.iOS.buildNumber = buildNumberString;
+            PlayerSettings.Android.bundleVersionCode = bundleVersionCode;
+            
+            logBuilder.Append("\t Parameters build number : ");
+            logBuilder.Append(buildNumber);
+            logBuilder.AppendLine();
+
+            logBuilder.Append("\t PlayerSettings.bundleVersion : ");
+            logBuilder.Append(version);
+            logBuilder.AppendLine();
+            
+            logBuilder.Append("\t PlayerSettings.iOS.buildNumber : ");
+            logBuilder.Append(buildNumberString);
+            logBuilder.AppendLine();
+            
+            logBuilder.Append("\t PlayerSettings.Android.bundleVersionCode : ");
+            logBuilder.Append(bundleVersionCode);
+            logBuilder.AppendLine();
+            
+        }
+
+    }
+    
+}
---
+++ UnityBuild/Tests/Editor/VersionCommandTests.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace UniGreenModules.UnityBuild.Tests
+{
+    using Editor.ClientBuild.Commands.PreBuildCommands;
+    using NUnit.Framework;
+    using UnityEditor;
+
+    [TestFixture]
+    public class VersionCommandTests : MonoBehaviour
+    {
+
+        [Test]
+        public void GetAndroidVersionTest()
+        {
+            
+            //arrange
+
+            var buildHandler = new BuildVersionProvider();
+            var buildNumber = 100;
+            
+            //act
+            var version = buildHandler.GetBuildVersion(BuildTarget.Android,"1.0.2", buildNumber);
+
+            //assert
+            Assert.That(version,Is.EqualTo("1.0.2.100"));            
+
+        }
+        
+        [Test]
+        public void GetIosVersionTest()
+        {
+            
+            //arrange
+
+            var buildHandler = new BuildVersionProvider();
+            var buildNumber  = 100;
+            
+            //act
+            var version = buildHandler.GetBuildVersion(BuildTarget.iOS,"1.0.2", buildNumber);
+
+            //assert
+            Assert.That(version,Is.EqualTo("1.0.100"));            
+
+        }
+        
+    }
+}
