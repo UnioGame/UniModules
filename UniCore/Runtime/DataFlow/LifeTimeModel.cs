9a5f56ba4b171c0dd16d847b9fa84aad41d128f1
Mefodei <mefodeilink@gmail.com>
2019-06-04 01:01:52 +0300

    - update packages to unity 2019

--- UniCore/Runtime/DataFlow/LifeTimeModel.cs
+++ UniCore/Runtime/DataFlow/LifeTimeModel.cs
@@ -11,13 +11,27 @@
 
         public ILifeTime LifeTime => lifeTimeDefinition.LifeTime;
 
+        /// <summary>
+        /// despawn model with cleanup
+        /// </summary>
         public void MakeDespawn()
         {
-            lifeTimeDefinition.Release();
-            OnDespawn();
+            Release();
             this.Despawn();
         }
 
-        protected virtual void OnDespawn(){}
+        /// <summary>
+        /// cleanup item without despawn
+        /// </summary>
+        protected void Release()
+        {
+            lifeTimeDefinition.Release();
+            OnCleanUp();
+        }
+        
+        /// <summary>
+        /// custom cleanup action
+        /// </summary>
+        protected virtual void OnCleanUp(){}
     }
 }
