07087e078a22cc051628dd5e270ea9184692edd6
MikhailovSergey <mikhailovss@plavalaguna.ru>
2019-07-30 17:56:27 +0300

    - add int to string cache utils - add EnumValue utils

---
+++ UniCore/Runtime/Utils/EnumTypeValues.cs
@@ -0,0 +1,27 @@
+﻿namespace UniGreenModules.UniCore.Runtime.Utils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Random = UnityEngine.Random;
+
+    public static class EnumValue <T>
+    {
+        
+        public static readonly List<T> Values =
+            Enum.GetValues(typeof(T)).
+                OfType<T>().ToList();
+    
+        public static readonly List<string> Names =
+            Enum.GetNames(typeof(T)).ToList();
+    
+
+        public static T GetRandom()
+        {
+            var index = Random.Range(0, Values.Count);
+            return Values[index];
+        }
+
+    }
+
+}
