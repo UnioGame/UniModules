39847e051596d8bb27a7b4ef483b8c031a6a2194
MikhailovSergey <mikhailovss@plavalaguna.ru>
2019-09-04 16:45:49 +0300

    - add datetime core extension

---
+++ UniCore/Runtime/DateTime/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace GBG.Utils.DateTimeUtils
+{
+    using System;
+
+    public static class DateTimeExtensions
+    {
+        public static DateTime UnitTime = new DateTime(1970, 1, 1);
+        
+        public static int ToUnixTimestamp(this DateTime date)
+        {
+            var unixTimestamp = (int)(date.ToUniversalTime().
+                Subtract(UnitTime)).TotalSeconds;
+            return unixTimestamp;
+        }
+
+    }
+}
