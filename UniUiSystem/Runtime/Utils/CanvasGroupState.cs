734e5bb5f2dea1f1ed7f57ad8e919f8e98cbb9bf
MikhailovSergey <mikhailovss@plavalaguna.ru>
2019-09-04 17:03:34 +0300

    - add uisystem utils

---
+++ UniUiSystem/Runtime/Utils/CanvasGroupState.cs
@@ -0,0 +1,10 @@
+namespace GBG.UiUtils.Runtime
+{
+    public struct CanvasGroupState
+    {
+        public float Alpha;
+        public bool  Interactable;
+        public bool  BlockRaycasts;
+        public bool  IgnoreParent;
+    }
+}
