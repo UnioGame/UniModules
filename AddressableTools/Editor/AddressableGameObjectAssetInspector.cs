6a04d7a633859d9deb18095a86d0970480958969
MikhailovSergey <mikhailovss@plavalaguna.ru>
2019-10-06 16:25:22 +0300

    - update packages

--- AddressableTools/Editor/AddressableGameObjectAssetInspector.cs
+++ AddressableTools/Editor/AddressableGameObjectAssetInspector.cs
@@ -1,8 +1,6 @@
 namespace UniGreenModules.AddressableTools.Editor
 {
-    using UnityEditor;
     using UnityEngine;
-    using UnityEngine.AddressableAssets;
 
     //[CustomPropertyDrawer(typeof(AssetReferenceGameObject), true)]
     public class AddressableGameObjectAssetInspector : AddressableAssetInspector<GameObject>{}
