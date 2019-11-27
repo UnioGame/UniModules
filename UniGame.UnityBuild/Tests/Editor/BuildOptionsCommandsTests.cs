ceb64356941bd625bcbf5d7890ac11366f701344
Mefodei <mefodei.link@gmail.com>
2019-11-27 19:22:36 +0300

    - update build pipeline

---
+++ UniGame.UnityBuild/Tests/Editor/BuildOptionsCommandsTests.cs
@@ -0,0 +1,46 @@
+﻿//using NSubstitute;
+//using NUnit.Framework;
+//using UniGreenModules.UnityBuild.Editor.ClientBuild;
+//using UniGreenModules.UnityBuild.Editor.ClientBuild.Commands.PreBuildCommands;
+//using UniGreenModules.UnityBuild.Editor.ClientBuild.Interfaces;
+//using UnityEditor;
+//using UnityEngine;
+//
+//[TestFixture]
+//public class BuildOptionsCommandsTests : MonoBehaviour
+//{
+//
+//    [Test]
+//    public void BuildOptionsParsingTest()
+//    {
+//        //arrange
+//        var optionsCommand = new BuildOptionsCommand();
+//        var parameterBuildOptions = BuildOptions.None;
+//        
+//        var resultBuildOptions = BuildOptions.None;
+//        resultBuildOptions |= BuildOptions.Development;
+//        resultBuildOptions |= BuildOptions.ConnectWithProfiler;
+//        
+//        var arguments = NSubstitute.Substitute.For<IArgumentsProvider>();
+//        arguments.Contains("-development").Returns(true);
+//        arguments.Contains("-connectwithprofiler").Returns(true);
+//        
+//        var parameters = NSubstitute.Substitute.For<IBuildParameters>();
+//        parameters.SetBuildOptions(Arg.Do<BuildOptions>(x => parameterBuildOptions = x), Arg.Any<bool>());
+//        
+//        var configuration = NSubstitute.Substitute.For<IUniBuilderConfiguration>();
+//        configuration.Arguments.Returns(arguments);
+//        configuration.BuildParameters.Returns(parameters);
+//        
+//        //act
+//
+//        optionsCommand.Execute(configuration);
+//
+//        //assert
+//        Assert.That(resultBuildOptions == parameterBuildOptions);
+//        
+//    }
+//   
+//    
+//    
+//}
---
+++ UniGame.UnityBuild/Tests/Editor/BuildParametersTests.cs
@@ -0,0 +1,71 @@
+﻿//using NUnit.Framework;
+//
+//namespace UniGreenModules.UnityBuild.Tests.Editor
+//{
+//    using UnityBuild.Editor.ClientBuild;
+//    using UnityBuild.Editor.ClientBuild.Interfaces;
+//    using UnityEditor;
+//
+//    [TestFixture]
+//    public class BuildParametersTests
+//    {
+//        [Test]
+//        public void ApplyBuildOptionsOverride()
+//        {
+//            //arrange
+//                        
+//            var options1 = BuildOptions.None;
+//            options1 |= BuildOptions.Development;
+//            options1 |= BuildOptions.ConnectWithProfiler;
+//            
+//            var options2 = BuildOptions.None;
+//            options2 |= BuildOptions.Reserved1;
+//            options2 |= BuildOptions.StrictMode;
+//
+//            var buildTarget = BuildTarget.Android;
+//            var buildTargetGroup = BuildTargetGroup.Android;
+//            var arguments = NSubstitute.Substitute.For<IArgumentsProvider>();
+//
+//            var buildParameters = new BuildParameters(buildTarget,buildTargetGroup,arguments);
+//
+//            //action
+//            buildParameters.SetBuildOptions(options2,false);
+//            buildParameters.SetBuildOptions(options1,true);
+//            
+//            //assert
+//            Assert.That(buildParameters.BuildOptions == options1);
+//            
+//       }
+//    
+//        
+//        [Test]
+//        public void ApplyBuildOptionsConcat()
+//        {
+//            //arrange
+//                        
+//            var options1 = BuildOptions.None;
+//            options1 |= BuildOptions.Development;
+//            options1 |= BuildOptions.ConnectWithProfiler;
+//            
+//            var options2 = BuildOptions.None;
+//            options2 |= BuildOptions.Reserved1;
+//            options2 |= BuildOptions.StrictMode;
+//
+//            var resultOptions = options1 | options2;
+//            
+//            var buildTarget      = BuildTarget.Android;
+//            var buildTargetGroup = BuildTargetGroup.Android;
+//            var arguments        = NSubstitute.Substitute.For<IArgumentsProvider>();
+//
+//            var buildParameters = new BuildParameters(buildTarget,buildTargetGroup,arguments);
+//
+//            //action
+//            buildParameters.SetBuildOptions(options2,false);
+//            buildParameters.SetBuildOptions(options1,false);
+//            
+//            //assert
+//            Assert.That(buildParameters.BuildOptions == resultOptions);
+//            
+//        }
+//    }
+//}
