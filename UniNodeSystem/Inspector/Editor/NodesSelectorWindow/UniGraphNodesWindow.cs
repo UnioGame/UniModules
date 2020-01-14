f64b42c65769cd722dde7653091f93be0e937796
mikhailovs <mikhailovs@taktikagames.ru>
2020-01-14 19:21:49 +0300

    - update node search window

--- UniNodeSystem/Inspector/Editor/NodesSelectorWindow/UniGraphNodesWindow.cs
+++ UniNodeSystem/Inspector/Editor/NodesSelectorWindow/UniGraphNodesWindow.cs
@@ -17,29 +17,16 @@ namespace UniGreenModules.UniNodeSystem.Inspector.Editor.NodesSelectorWindow
 
         public void OnEnable()
         {
-            // A stylesheet can be added to a VisualElement.
-            // The style will be applied to the VisualElement and all of its children.
-            var styleSheet = Resources.Load<StyleSheet>("UniGraphNodesWindowStyle");
-            
+
             // Each editor window contains a root VisualElement object
             var root = rootVisualElement;
 
             root.AddToClassList(windowClassName);
-            root.styleSheets.Add(styleSheet);
             
-            // VisualElements objects can contain other VisualElement following a tree hierarchy.
-            VisualElement label = new Label("Hello World! From C#");
-            root.Add(label);
-
-            // Import UXML
             var           visualTree    = Resources.Load<VisualTreeAsset>("UniGraphNodesWindow");
             VisualElement labelFromUXML = visualTree.CloneTree();
             root.Add(labelFromUXML);
 
-            VisualElement labelWithStyle = new Label("Hello World! With Style");
-            labelWithStyle.AddToClassList("unigame-nodes-label");
-            labelWithStyle.styleSheets.Add(styleSheet);
-            root.Add(labelWithStyle);
         }
     }
 }
