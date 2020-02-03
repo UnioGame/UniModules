cea0320d9abee0e53185f98cda2c0a2a5fd9a255
mikhailovs <mikhailovs@taktikagames.ru>
2020-02-03 18:57:37 +0300

    - move node based game flow to node system repo

---
+++ UniGame.Context/Runtime/Abstract/AsyncObjectDataSource.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace UniGreenModules.UniGame.Context.Runtime.Context
+{
+    using UniContextData.Runtime.Interfaces;
+    using UniCore.Runtime.Interfaces;
+    using UniRx.Async;
+
+    public abstract class AsyncObjectDataSource : MonoBehaviour, IAsyncContextDataSource
+    {
+
+        public abstract UniTask<IContext> RegisterAsync(IContext context);
+    }
+}
