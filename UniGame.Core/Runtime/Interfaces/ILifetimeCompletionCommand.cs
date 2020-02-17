dfa849015b0bf22133b234f76e2e19b240abb79e
mikhailovs <mikhailovs@taktikagames.ru>
2020-02-17 14:14:39 +0300

    - start game flow node changes

---
+++ UniGame.Core/Runtime/Interfaces/ILifetimeCompletionCommand.cs
@@ -0,0 +1,9 @@
+﻿using UniGreenModules.UniCore.Runtime.Interfaces;
+
+namespace UniGreenModules.UniGame.Core.Runtime.Interfaces
+{
+    public interface ILifeTimeCompletionCommand : ILifeTimeCommand, ICompletionSource
+    {
+    
+    }
+}
