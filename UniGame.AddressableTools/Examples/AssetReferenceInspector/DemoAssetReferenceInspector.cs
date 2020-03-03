1756ac0c719257eca94122bec027c41af48dbe4d
mikhailovs <mikhailovs@taktikagames.ru>
2020-03-03 16:17:00 +0300

    - update addressables reference inspector add List/Array support

---
+++ UniGame.AddressableTools/Examples/AssetReferenceInspector/DemoAssetReferenceInspector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace UniGreenModules.UniGame.AddressableTools.Editor.AddressablesAssetDrawer.Examples
+{
+        using System.Collections.Generic;
+        using Runtime.Attributes;
+    using UnityEngine.AddressableAssets;
+
+    public class DemoAssetReferenceInspector : MonoBehaviour
+    {
+#if ODIN_INSPECTOR
+        [Sirenix.OdinInspector.DrawWithUnity]
+#endif
+        [ShowAssetReference]
+        public AssetReference Demo1;
+
+#if ODIN_INSPECTOR
+        [Sirenix.OdinInspector.DrawWithUnity]
+#endif
+        [ShowAssetReference]
+        public List<AssetReference> DemoList = new List<AssetReference>();
+        
+    }
+}
