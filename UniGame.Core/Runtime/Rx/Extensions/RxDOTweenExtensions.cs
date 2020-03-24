9680e2992a90a9bb837d22209047cd1eb64d382f
Vorontcov Gleb <voroncovg@taktikagames.ru>
2020-03-24 16:44:52 +0300

    added extensions for DOTween Pro (don't used now)

--- UniGame.Core/Runtime/Rx/Extensions/RxDOTweenExtensions.cs
+++ UniGame.Core/Runtime/Rx/Extensions/RxDOTweenExtensions.cs
@@ -30,5 +30,111 @@ namespace UniGreenModules.UniCore.Runtime.Rx.Extensions
                 return Disposable.Create(() => sequence.Kill());
             });
         }
+        
+        #if false //DOTweenPro Extensions
+    static public IObservable<DOTweenAnimation> DOPlayAsObservable(
+        this DOTweenAnimation animation,
+        bool rewind = false)
+    {
+        return Observable.Create<DOTweenAnimation>(o =>
+        {
+            if (rewind) animation.DORewind();
+
+            animation.tween.OnComplete(() =>
+            {
+                o.OnNext(animation);
+                o.OnCompleted();
+            });
+            animation.DOPlay();
+            return Disposable.Empty;
+        });
+    }
+
+    static public IObservable<DOTweenAnimation> DOPlayByIdAsObservable(
+        this DOTweenAnimation animation,
+        string id,
+        bool rewind = false)
+    {
+        return Observable.Create<DOTweenAnimation>(o =>
+        {
+            if (rewind) animation.DORewind();
+
+            animation.tween.OnComplete(() =>
+            {
+                o.OnNext(animation);
+                o.OnCompleted();
+            });
+            animation.DOPlayById(id);
+            return Disposable.Empty;
+        });
+    }
+
+    static public IObservable<DOTweenAnimation> DOPlayAllByIdAsObservable(
+        this DOTweenAnimation animation,
+        string id,
+        bool rewind = false)
+    {
+        return Observable.Create<DOTweenAnimation>(o =>
+        {
+            if (rewind) animation.DORewind();
+
+            animation.tween.OnComplete(() =>
+            {
+                o.OnNext(animation);
+                o.OnCompleted();
+            });
+            animation.DOPlayAllById(id);
+            return Disposable.Empty;
+        });
+    }
+
+    static public IObservable<DOTweenAnimation> DORestartAsObservable(
+        this DOTweenAnimation animation,
+        bool fromHere = false)
+    {
+        return Observable.Create<DOTweenAnimation>(o =>
+        {
+            animation.tween.OnComplete(() =>
+            {
+                o.OnNext(animation);
+                o.OnCompleted();
+            });
+            animation.DORestart(fromHere);
+            return Disposable.Empty;
+        });
+    }
+
+    static public IObservable<DOTweenAnimation> DORestartByIdAsObservable(
+        this DOTweenAnimation animation,
+        string id)
+    {
+        return Observable.Create<DOTweenAnimation>(o =>
+        {
+            animation.tween.OnComplete(() =>
+            {
+                o.OnNext(animation);
+                o.OnCompleted();
+            });
+            animation.DORestartById(id);
+            return Disposable.Empty;
+        });
+    }
+
+    static public IObservable<DOTweenAnimation> DORestartAllByIdAsObservable(
+        this DOTweenAnimation animation,
+        string id)
+    {
+        return Observable.Create<DOTweenAnimation>(o =>
+        {
+            animation.tween.OnComplete(() =>
+            {
+                o.OnNext(animation);
+                o.OnCompleted();
+            });
+            animation.DORestartAllById(id);
+            return Disposable.Empty;
+        });
+    }
+#endif //DOTweenPro Extensions
     }
 }
