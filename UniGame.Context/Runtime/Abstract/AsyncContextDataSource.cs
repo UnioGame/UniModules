791438a843a5db6d374b91ebd3fbbdc333f86bba
Mefodei <mefodei.link@gmail.com>
2020-03-27 16:23:45 +0300

    - add DIsposableScriptableObject

--- UniGame.Context/Runtime/Abstract/AsyncContextDataSource.cs
+++ UniGame.Context/Runtime/Abstract/AsyncContextDataSource.cs
@@ -1,47 +1,17 @@
 ﻿namespace UniGreenModules.UniGame.Context.Runtime.Interfaces
 {
-    using SerializableContext.Runtime.Abstract;
     using UniContextData.Runtime.Interfaces;
-    using UniCore.Runtime.DataFlow;
-    using UniCore.Runtime.DataFlow.Interfaces;
     using UniCore.Runtime.Interfaces;
     using UniRx.Async;
-    using UnityEngine;
 
     public abstract class AsyncContextDataSource : 
-        ScriptableObject, 
-        IAsyncContextDataSource,
-        ILifeTimeContext,
-        IResourceDisposable
+        DisposableScriptableObject, 
+        IAsyncContextDataSource
+
     {
-        #region inspector
-        
-        [Tooltip("Unload context asset on SO unloading")]
-        public bool disposeOnUnload = true;
-        
-        #endregion
-        
-        private LifeTimeDefinition lifeTimeDefinition;
-        
-        public ILifeTime LifeTime => lifeTimeDefinition.LifeTime;
-        
 
         public abstract UniTask<IContext> RegisterAsync(IContext context);
-        
-        public void Dispose() => lifeTimeDefinition.Terminate();
-        
-        private void OnEnable()
-        {
-            lifeTimeDefinition = new LifeTimeDefinition();
-            if (disposeOnUnload) {
-                lifeTimeDefinition.LifeTime.AddDispose(this);
-            }
-            OnSourceEnable(LifeTime);
-        }
 
-        private void OnDisable() => Dispose();
-
-        protected virtual void OnSourceEnable(ILifeTime lifeTime) {}
         
     }
 }
