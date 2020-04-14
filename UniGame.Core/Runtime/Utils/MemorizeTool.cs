0628cf3079a9d0c3d0edf890665f7bdd8d596a5d
Mefodei <mefodei.link@gmail.com>
2020-04-14 13:39:21 +0300

    - update context window api

--- UniGame.Core/Runtime/Utils/MemorizeTool.cs
+++ UniGame.Core/Runtime/Utils/MemorizeTool.cs
@@ -2,24 +2,29 @@
 {
     using System;
     using System.Collections.Generic;
+    using UnityEditor;
 
     public class MemorizeTool
     {
-
         public static Func<TKey,TData> Create<TKey,TData>(Func<TKey,TData> factory) {
 
             var cache = new Dictionary<TKey,TData>(16);
 
-            return x => {
-
+            TData CacheMapFunc(TKey x)
+            {
                 if (cache.TryGetValue(x, out var value) == false || value == null) {
-                    value = factory(x);
+                    value    = factory(x);
                     cache[x] = value;
                 }
-
                 return value;
+            }
 
-            };
+            void ClearCache() => cache.Clear();
+
+            //clean up cache if Assembly Reload
+            AssemblyReloadEvents.beforeAssemblyReload += ClearCache;
+            
+            return CacheMapFunc;
 
         }
 
