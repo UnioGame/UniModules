53a117beac246dd96f50f5cf2d49c03d590a4142
Vorontcov Gleb <vorontcovg@taktikagames.ru>
2020-04-29 11:44:19 +0300

    replaced extensions and helper to core module

---
+++ UniGame.Core/EditorTools/Editor/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace UniModules.UniGame.Core.EditorTools.Editor
+{
+    using System;
+
+    public static class StringExtensions
+    {
+        public static string ToPathFromAssets(this string path)
+        {
+            if (path == null)
+                return string.Empty;
+            
+            var index = path.IndexOf("Assets", StringComparison.Ordinal);
+            if(index >= 0) {
+                path = path.Substring(index);
+            }
+            
+            return path;
+        }
+    }
+}
