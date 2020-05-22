f82063cae40c0e49c0b8189f82abc8d789e167a8
Vorontcov Gleb <vorontcovg@taktikagames.ru>
2020-05-22 19:25:22 +0300

    changed names

--- UniGame.DoTweenRoutines/Runtime/DOTweenRoutineExtensions.cs
+++ UniGame.DoTweenRoutines/Runtime/DOTweenRoutineExtensions.cs
@@ -5,42 +5,42 @@
 
     public static class DOTweenRoutineExtensions
     {
-        public static IEnumerator WaitForCompletion(this Tween t)
+        public static IEnumerator WaitForCompletionTween(this Tween t)
         {
             while (t.active && !t.IsComplete()) {
                 yield return null;
             }
         }
 
-        public static IEnumerator WaitForRewind(this Tween t)
+        public static IEnumerator WaitForRewindTween(this Tween t)
         {
             while (t.active && (!t.playedOnce || t.position * (t.CompletedLoops() + 1) > 0.0)) {
                 yield return null;
             }
         }
 
-        public static IEnumerator WaitForKill(this Tween t)
+        public static IEnumerator WaitForKillTween(this Tween t)
         {
             while (t.active) {
                 yield return null;
             }
         }
 
-        public static IEnumerator WaitForElapsedLoop(this Tween t, int elapsedLoops)
+        public static IEnumerator WaitForElapsedLoopTween(this Tween t, int elapsedLoops)
         {
             while (t.active && t.CompletedLoops() < elapsedLoops) {
                 yield return null;
             }
         }
 
-        public static IEnumerator WaitForPosition(this Tween t, float position)
+        public static IEnumerator WaitForPositionTween(this Tween t, float position)
         {
             while (t.active && t.position * (t.CompletedLoops() + 1) < position) {
                 yield return null;
             }
         }
 
-        public static IEnumerator WaitForStart(this Tween t)
+        public static IEnumerator WaitForStartTween(this Tween t)
         {
             while (t.active && !t.playedOnce) {
                 yield return null;
