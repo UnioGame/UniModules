fc5963834fa596de611a3b3da94be87b2661d612
Vorontcov Gleb <vorontcovg@taktikagames.ru>
2020-05-22 19:17:41 +0300

    added dotween routine extensions

---
+++ UniGame.DoTweenRoutines/Runtime/DOTweenRoutineExtensions.cs
@@ -0,0 +1,50 @@
+﻿namespace UniModules.UniGame.DoTweenRoutines.Runtime
+{
+    using System.Collections;
+    using DG.Tweening;
+
+    public static class DOTweenRoutineExtensions
+    {
+        public static IEnumerator WaitForCompletion(this Tween t)
+        {
+            while (t.active && !t.IsComplete()) {
+                yield return null;
+            }
+        }
+
+        public static IEnumerator WaitForRewind(this Tween t)
+        {
+            while (t.active && (!t.playedOnce || t.position * (t.CompletedLoops() + 1) > 0.0)) {
+                yield return null;
+            }
+        }
+
+        public static IEnumerator WaitForKill(this Tween t)
+        {
+            while (t.active) {
+                yield return null;
+            }
+        }
+
+        public static IEnumerator WaitForElapsedLoop(this Tween t, int elapsedLoops)
+        {
+            while (t.active && t.CompletedLoops() < elapsedLoops) {
+                yield return null;
+            }
+        }
+
+        public static IEnumerator WaitForPosition(this Tween t, float position)
+        {
+            while (t.active && t.position * (t.CompletedLoops() + 1) < position) {
+                yield return null;
+            }
+        }
+
+        public static IEnumerator WaitForStart(this Tween t)
+        {
+            while (t.active && !t.playedOnce) {
+                yield return null;
+            }
+        }
+    }
+}
