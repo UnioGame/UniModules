21dc7ce6507ea8d9b0b7b6b7ad30bd7b24e921dd
MikhailovSSergei <mikhailovs@taktikagames.ru>
2020-06-01 18:08:56 +0300

    - update gameflow commit

---
+++ UniGame.UiElements/Examples/Editor/DemoTypeDrawer/DemoTypeAsset.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace UniModules.UniGame.UiElements.Examples.Editor
+{
+    using System.Collections.Generic;
+
+    [CreateAssetMenu(menuName = "UniGame/UiElements/Examples/DemoTypeAsset",fileName = "DemoTypeAsset")]
+    public class DemoTypeAsset : ScriptableObject
+    {
+        public List<Object> assets;
+
+        public Component ComponentValue;
+        public GameObject GameObjectValue;
+        public Vector3 Vector3Value = Vector3.right;
+        public Vector2 Vector2Value = Vector2.left;
+        public Sprite SpriteValue;
+        public Texture TextureValue;
+        public int IntValue;
+        public bool BoolValue;
+
+#if ODIN_INSPECTOR
+        [Sirenix.OdinInspector.Button]
+#endif
+        [ContextMenu("Show UniElements Demo Window")]
+        public void Show()
+        {
+            var targets = new List<object>() {
+                this,
+                GameObjectValue,
+                ComponentValue,
+                // assets,
+                // Vector2Value,
+                // Vector2Value,
+                // SpriteValue,
+                // TextureValue,
+                // IntValue,
+                // BoolValue,
+            };
+            DemoTypeDrawerWindow.Show(targets);
+        }
+    
+    
+    }
+}
