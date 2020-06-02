2e10130a6b062252e38b8653c2f1bfa31ab6bd9b
MikhailovSSergei <mikhailovs@taktikagames.ru>
2020-06-02 17:06:20 +0300

    - update Git cleaner version - update prefab utility

--- UniGame.Core/EditorTools/Editor/PrefabExtensions.cs
+++ UniGame.Core/EditorTools/Editor/PrefabExtensions.cs
@@ -1,30 +1,33 @@
 ﻿namespace UniModules.UniGame.Core.EditorTools.Editor
 {
     using AssetOperations;
-    using UnityEditor;
     using UnityEngine;
 
     public static class PrefabExtensions
     {
         public static bool TryApplyPrefab(this GameObject source, string path, out GameObject prefab)
         {
-            if (PrefabUtility.IsPartOfAnyPrefab(source) && AssetDatabaseHelper.TryGetAsset<GameObject>(path, out var result)) {
-                PrefabUtility.ApplyPrefabInstance(source, InteractionMode.AutomatedAction);
+#if UNITY_EDITOR
+            if (source && UnityEditor.PrefabUtility.IsPartOfAnyPrefab(source) && AssetDatabaseHelper.TryGetAsset<GameObject>(path, out var result)) {
+                UnityEditor.PrefabUtility.ApplyPrefabInstance(source, UnityEditor.InteractionMode.AutomatedAction);
                 prefab = result;
                 return true;
             }
-
+#endif
             prefab = null;
             return false;
         }
 
         public static bool TryApplyPrefab(this GameObject source)
         {
-            if (PrefabUtility.IsPartOfAnyPrefab(source)) {
-                PrefabUtility.ApplyPrefabInstance(source, InteractionMode.AutomatedAction);
+            if (!source)
+                return false;
+#if UNITY_EDITOR
+            if (UnityEditor.PrefabUtility.IsPartOfAnyPrefab(source)) {
+                UnityEditor.PrefabUtility.ApplyPrefabInstance(source, UnityEditor.InteractionMode.AutomatedAction);
                 return true;
             }
-
+#endif
             return false;
         }
     }
