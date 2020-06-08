f4d888e21cafc0939f380fc3b292ce53783691f2
Vorontcov Gleb <vorontcovg@taktikagames.ru>
2020-06-08 14:23:37 +0300

    replaced uint to string converter

---
+++ UniGame.Core/Runtime/Utils/TypeToStringConverter.cs
@@ -0,0 +1,20 @@
+﻿namespace UniGreenModules.UniCore.Runtime.Utils
+{
+    using System;
+
+    public static class TypeToStringConverter
+    {
+        private static readonly Func<int, string> IntConverter = MemorizeTool.Create<int,string>(x => x.ToString());
+        private static readonly Func<uint, string> UIntConverter = MemorizeTool.Create<uint, string>(x => x.ToString());
+
+        public static string ToStringFromCache(this int value)
+        {
+            return IntConverter.Invoke(value);
+        }
+        
+        public static string ToStringFromCache(this uint value)
+        {
+            return UIntConverter.Invoke(value);
+        }
+    }
+}
