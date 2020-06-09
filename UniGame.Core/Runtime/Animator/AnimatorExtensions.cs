3f8a06b92dc59e7adce77040897eba116923b8ee
Vorontcov Gleb <vorontcovg@taktikagames.ru>
2020-06-09 13:23:02 +0300

    added animator extension

---
+++ UniGame.Core/Runtime/Animator/AnimatorExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace UniModules.UniGame.Core.Runtime.Animator
+{
+    using System.Collections;
+    using UnityEngine;
+
+    public static class AnimatorExtensions
+    {
+        public static IEnumerator PlayAndWaitForEnd(this Animator animator, int stateHash, int layer = 0)
+        {
+            while (animator.GetCurrentAnimatorStateInfo(layer).shortNameHash != stateHash) {
+                yield return null;
+            }
+
+            var currentTime = 0.0f;
+            var waitTime = animator.GetCurrentAnimatorStateInfo(layer).length;
+
+            while (currentTime < waitTime) {
+                currentTime += Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
+}
