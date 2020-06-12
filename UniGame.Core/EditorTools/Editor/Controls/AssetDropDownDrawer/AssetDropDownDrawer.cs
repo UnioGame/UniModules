84231ffed7c6bc51515fbedd0e16a24986a719f8
Mefodei <mefodei.link@gmail.com>
2020-06-13 02:08:51 +0300

    - fix drop down perfomance

--- UniGame.Core/EditorTools/Editor/Controls/AssetDropDownDrawer/AssetDropDownDrawer.cs
+++ UniGame.Core/EditorTools/Editor/Controls/AssetDropDownDrawer/AssetDropDownDrawer.cs
@@ -1,40 +1,42 @@
 ﻿namespace UniGreenModules.UniGame.Core.EditorTools.Editor.Controls.AssetDropDownControl
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using Runtime.Attributes;
     using UniCore.EditorTools.Editor.AssetOperations;
+    using UniCore.Runtime.Utils;
     using UnityEditor;
-    using UnityEditor.UIElements;
     using UnityEngine;
-    using UnityEngine.UIElements;
+    using Object = UnityEngine.Object;
 
     [CustomPropertyDrawer(typeof(AssetDropDownAttribute))]
     public class AssetDropDownDrawer : PropertyDrawer
     {
         private const string emptyValue = "none";
         
-        private static List<Object> assets = new List<Object>();
         private static List<string> assetsItems = new List<string>();
+        private static Func<AssetDropDownAttribute, List<Object>> typeAssets = MemorizeTool.Create<AssetDropDownAttribute,List<Object>>(FindFiltered);
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             var targetAttribute = attribute as AssetDropDownAttribute;
-            
+
             var filterType = targetAttribute.FilterType == null ?
                 fieldInfo.FieldType : targetAttribute.FilterType;
             
             var folderFilter = targetAttribute.FolderFilter;
             
-            assets.Clear();
             assetsItems.Clear();
             assetsItems.Add(emptyValue);
             
-            AssetEditorTools.FindAssets(assets, filterType,
-                string.IsNullOrEmpty(folderFilter) ? 
-                    null :
-                    new []{folderFilter});
+            var searchTarget = new AssetDropDownAttribute() {
+                FilterType = filterType,
+                FolderFilter = folderFilter,
+                FoldOutOpen = targetAttribute.FoldOutOpen
+            };
 
+            var assets = typeAssets(searchTarget);
             
             var target = property.objectReferenceValue;
             var currentValue = assets.FirstOrDefault(x => target == x);
@@ -63,8 +65,15 @@
             property.serializedObject.ApplyModifiedProperties();
 
         }
-        
-        
+
+        private static List<Object> FindFiltered(AssetDropDownAttribute targetAttribute)
+        {
+            var searchAsset = new List<Object>();
+            return AssetEditorTools.FindAssets(searchAsset, targetAttribute.FilterType,
+                string.IsNullOrEmpty(targetAttribute.FolderFilter) ? 
+                    null :
+                    new []{targetAttribute.FolderFilter});
+        }
         
     }
 }
