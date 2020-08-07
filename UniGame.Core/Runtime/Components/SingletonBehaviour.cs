503540c5fad5d529e892e73e2f4b6fa36ce02174
Vorontcov Gleb <vorontcovg@taktikagames.ru>
2020-08-07 18:36:02 +0300

    added generic singleton behaviour

---
+++ UniGame.Core/Runtime/Components/SingletonBehaviour.cs
@@ -0,0 +1,47 @@
+﻿namespace UniModules.UniGame.Core.Runtime.Components
+{
+    using UniCore.Runtime.ProfilerTools;
+    using UnityEngine;
+
+    public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static bool _shuttingDown;
+        private static readonly object _lock = new object();
+        private static T _instance;
+
+        public static T Instance {
+            get {
+                if (_shuttingDown) {
+                    GameLog.LogWarning($"[Singleton]: Instance of {typeof(T)} already destroyed.");
+                    return null;
+                }
+
+                lock (_lock) {
+                    if (_instance == null) {
+                        _instance = FindObjectOfType<T>();
+
+                        if (_instance == null) {
+                            var singletonObject = new GameObject();
+                            _instance = singletonObject.AddComponent<T>();
+                            singletonObject.name = $"{typeof(T)} (Singleton)";
+                            
+                            DontDestroyOnLoad(singletonObject);
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        protected virtual void OnApplicationQuit()
+        {
+            _shuttingDown = true;
+        }
+
+        protected virtual void OnDestroy()
+        {
+            _shuttingDown = true;
+        }
+    }
+}
