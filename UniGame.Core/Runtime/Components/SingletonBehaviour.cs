6707389c73f3c6f7ff3bafc30bd7bd5ee8c8e273
Vorontcov Gleb <vorontcovg@taktikagames.ru>
2020-08-10 12:18:12 +0300

    rewrote singleton behaviour

--- UniGame.Core/Runtime/Components/SingletonBehaviour.cs
+++ UniGame.Core/Runtime/Components/SingletonBehaviour.cs
@@ -1,14 +1,19 @@
 ﻿namespace UniModules.UniGame.Core.Runtime.Components
 {
     using UniCore.Runtime.ProfilerTools;
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
     using UnityEngine;
 
-    public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    public class SingletonBehaviour<T> : MonoBehaviour where T : SingletonBehaviour<T>
     {
         private static bool _shuttingDown;
         private static readonly object _lock = new object();
         private static T _instance;
 
+        public static bool Exists => _instance != null;
+        
         public static T Instance {
             get {
                 if (_shuttingDown) {
@@ -18,20 +23,30 @@
 
                 lock (_lock) {
                     if (_instance == null) {
-                        _instance = FindObjectOfType<T>();
-
-                        if (_instance == null) {
-                            var singletonObject = new GameObject();
-                            _instance = singletonObject.AddComponent<T>();
-                            singletonObject.name = $"{typeof(T)} (Singleton)";
-                            
-                            DontDestroyOnLoad(singletonObject);
-                        }
+                        _instance = FindObjectOfType<T>() ?? CreateInstance();
                     }
                 }
 
                 return _instance;
             }
+        }
+
+        public static void DestroySingleton()
+        {
+            if (Exists) {
+                DestroyImmediate(Instance.gameObject);
+                _instance = null;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance != null && _instance != this) {
+                DestroyImmediate(gameObject);
+                return;
+            }
+            
+            _instance = this as T;
         }
 
         protected virtual void OnApplicationQuit()
@@ -43,5 +58,48 @@
         {
             _shuttingDown = true;
         }
+
+        protected virtual void OnEnable()
+        {
+#if UNITY_EDITOR
+            EditorApplication.playModeStateChanged += EditorApplicationOnplayModeStateChanged;
+#endif
+        }
+
+        protected virtual void OnDisable()
+        {
+#if UNITY_EDITOR
+            EditorApplication.playModeStateChanged -= EditorApplicationOnplayModeStateChanged;
+#endif
+        }
+
+        protected virtual string GetGameObjectName() => typeof(T).Name;
+
+        private static T CreateInstance()
+        {
+            var singletonObject = new GameObject();
+
+            if (Application.isPlaying) {
+                DontDestroyOnLoad(singletonObject);
+                singletonObject.hideFlags = HideFlags.DontSave;
+            }
+            else {
+                singletonObject.hideFlags = HideFlags.HideAndDontSave;
+            }
+
+            var instance = singletonObject.AddComponent<T>();
+            singletonObject.name = instance.GetGameObjectName();
+
+            return instance;
+        }
+        
+#if UNITY_EDITOR
+        private void EditorApplicationOnplayModeStateChanged(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.ExitingPlayMode) {
+                DestroySingleton();
+            }
+        }
+#endif
     }
 }
