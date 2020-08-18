b4ff9127a0ff624490d910ed8c3d8d0158c29f74
Mefodei <mefodei.link@gmail.com>
2020-08-18 16:32:51 +0300

    - move url parameters to extension

--- UniGame.Core/Runtime/Utils/UriExtensions.cs
+++ UniGame.Core/Runtime/Utils/UriExtensions.cs
@@ -1,9 +1,11 @@
 ﻿namespace UniModules.UniGame.Core.Runtime.Utils
 {
     using System;
+    using System.Collections.Generic;
     using System.Collections.Specialized;
     using System.Globalization;
     using System.Text;
+    using UnityEngine.Networking;
 
     public static class UriExtensions
     {
@@ -16,6 +18,22 @@
             uriBuilder.Query = query.ToString();
 
             return uriBuilder.Uri;
+        }
+
+        public static string CombineUrlParameters(this string url,IDictionary<string,string> parameters)
+        {
+            var urlParameters = new List<string>();
+            foreach (var parameter in parameters) {
+                var keyValue = $"{parameter.Key}={UnityWebRequest.EscapeURL(parameter.Value)}";
+                urlParameters.Add(keyValue);
+            }
+
+            var targetUrl = url;
+            if (urlParameters.Count > 0) {
+                targetUrl += $"?{string.Join("&",urlParameters)}";
+            }
+
+            return targetUrl;
         }
     }
 }
