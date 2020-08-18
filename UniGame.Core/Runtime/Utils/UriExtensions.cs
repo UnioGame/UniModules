b37d9321eb800ba82aea08662673b8d1a60c7982
Vorontcov Gleb <vorontcovg@taktikagames.ru>
2020-08-18 14:14:56 +0300

    replaced http utility and uri extensions

---
+++ UniGame.Core/Runtime/Utils/UriExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace UniModules.UniGame.Core.Runtime.Utils
+{
+    using System;
+    using System.Collections.Specialized;
+    using System.Globalization;
+    using System.Text;
+
+    public static class UriExtensions
+    {
+        public static Uri AddTimeStampToUri(this Uri address)
+        {
+            var uriBuilder = new UriBuilder(address);
+            var query = new NameValueCollection();
+            HttpUtility.ParseQueryString(uriBuilder.Query, Encoding.UTF8, query);
+            query["x"] = DateTime.Now.ToFileTime().ToString(CultureInfo.InvariantCulture);
+            uriBuilder.Query = query.ToString();
+
+            return uriBuilder.Uri;
+        }
+    }
+}
