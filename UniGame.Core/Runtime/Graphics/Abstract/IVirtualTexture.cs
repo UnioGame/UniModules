4150be5c7f8eb0bcae48202c2cd58c107be5237e
Vorontcov Gleb <vorontcovg@taktikagames.ru>
2020-08-24 19:22:51 +0300

    added virtual texture interface

---
+++ UniGame.Core/Runtime/Graphics/Abstract/IVirtualTexture.cs
@@ -0,0 +1,14 @@
+﻿namespace UniModules.UniGame.Core.Runtime.Graphics.Abstract
+{
+    using UnityEngine;
+
+    public interface IVirtualTexture
+    {
+        int Width  { get; }
+        int Height { get; }
+
+        int PixelCount { get; }
+        
+        Color GetPixelColor(float x, float y);
+    }
+}
