36abbf3a68a06975b88a2adfaf1d420fe9a764a6
Mefodei <mefodei.link@gmail.com>
2020-08-28 04:26:06 +0300

    https://taktikagames.atlassian.net/browse/HOGM-1959

--- UniGame.Core/Runtime/Utils/MemorizeTool.cs
+++ UniGame.Core/Runtime/Utils/MemorizeTool.cs
@@ -1,38 +1,28 @@
 ﻿namespace UniGreenModules.UniCore.Runtime.Utils
 {
     using System;
-    using System.Collections.Generic;
 
-    public class MemorizeTool
+    public static class MemorizeTool
     {
-
-        public static Func<TKey,TData> Create<TKey,TData>(Func<TKey,TData> factory, Action<TData> disposableAction = null) {
-
-            var cache = new Dictionary<TKey,TData>(16);
-
-            TData CacheMapFunc(TKey x)
-            {
-                if (cache.TryGetValue(x, out var value) == false || value == null) {
-                    value    = factory(x);
-                    cache[x] = value;
-                }
-                return value;
-            }
-
-            void ClearCache()
-            {
-                foreach (var data in cache) {
-                    disposableAction?.Invoke(data.Value);
-                }
-                cache.Clear();
-            }
+        
+        public static MemorizeItem<TKey,TData> Memorize<TKey,TData>(Func<TKey, TData> factory, Action<TData> disposableAction = null)
+        {
+            var cache = new MemorizeItem<TKey,TData>(factory,disposableAction);
             
 #if UNITY_EDITOR
             //clean up cache if Assembly Reload
-            UnityEditor.AssemblyReloadEvents.beforeAssemblyReload += ClearCache;
-            UnityEditor.EditorApplication.playModeStateChanged += x => ClearCache();
+            UnityEditor.AssemblyReloadEvents.beforeAssemblyReload += cache.Dispose;
+            UnityEditor.EditorApplication.playModeStateChanged    += x => cache.Dispose();
 #endif      
-            return CacheMapFunc;
+            
+            return cache;
+        }
+
+        public static Func<TKey,TData> Create<TKey,TData>(Func<TKey,TData> factory, Action<TData> disposableAction = null) {
+
+            var cache = Memorize(factory,disposableAction);
+
+            return cache.GetValue;
 
         }
 
