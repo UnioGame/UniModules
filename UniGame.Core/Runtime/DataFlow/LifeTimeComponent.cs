501d34c5ef0982d66c8b7da8064e0026c6731265
Mefodei <mefodei.link@gmail.com>
2020-09-19 17:40:05 +0300

    - add lifetime component - update submodules

---
+++ UniGame.Core/Runtime/DataFlow/LifeTimeComponent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace UniModules.UniGame.Core.Runtime.DataFlow
+{
+    using System;
+    using Interfaces;
+    using UniGreenModules.UniCore.Runtime.DataFlow;
+
+    public class LifeTimeComponent :
+        MonoBehaviour, 
+        ILifeTime
+    {
+        private readonly LifeTimeDefinition _lifeTime = new LifeTimeDefinition();
+
+        public ILifeTime AddCleanUpAction(Action cleanAction) => _lifeTime.AddCleanUpAction(cleanAction);
+
+        public ILifeTime AddDispose(IDisposable item) => _lifeTime.AddDispose(item);
+
+        public ILifeTime AddRef(object o) => _lifeTime.AddRef(o);
+
+        public bool IsTerminated => _lifeTime.IsTerminated;
+
+        private void OnDestroy()
+        {
+            _lifeTime.Terminate();
+        }
+    }
+}
