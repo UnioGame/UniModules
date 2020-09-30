9a22b2d6274778420fbe275f92ee4cf2bc108bd3
Mefodei <mefodei.link@gmail.com>
2020-10-01 02:44:22 +0300

    - add collection selector interface

--- UniGame.AddressableTools/Runtime/AssetReferencies/ScriptableObjectAssetReferenceT.cs
+++ UniGame.AddressableTools/Runtime/AssetReferencies/ScriptableObjectAssetReferenceT.cs
@@ -4,9 +4,6 @@ namespace UniGreenModules.UniGame.AddressableTools.Runtime.AssetReferencies
 {
     using System;
 
-#if ODIN_INSPECTOR
-    [Sirenix.OdinInspector.DontApplyToListElements]
-#endif
     [Serializable]
     public class ScriptableObjectAssetReferenceT<T> : DisposableAssetReference<T>
         where T : ScriptableObject
--- UniGame.Core/Runtime/Interfaces/ISelector.cs
+++ UniGame.Core/Runtime/Interfaces/ISelector.cs
@@ -1,9 +1,16 @@
 ﻿namespace UniGreenModules.UniCore.Runtime.Interfaces 
 {
+	using System.Collections.Generic;
+
 	public interface ISelector<TState> {
 
 		TState Select();
 
+	}
+	
+	public interface ICollectionSelector<T>
+	{
+		T Select(IEnumerable<T> collection);
 	}
 
 }
