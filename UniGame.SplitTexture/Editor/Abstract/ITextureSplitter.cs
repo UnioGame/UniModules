c49f5b1cbe3463696e5e739ba5c6c77c4cb7d159
Mefodei <mefodei.link@gmail.com>
2020-10-05 02:41:14 +0300

    - remove nested

--- UniGame.SplitTexture/Editor/Abstract/ITextureSplitter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UniModules.UniGame.SplitTexture.Editor.Abstract
-{
-    using UnityEngine;
-
-    public interface ITextureSplitter
-    {
-        Texture2D[] SplitTexture(Texture2D source, Vector2Int maxSize);
-    }
-}
