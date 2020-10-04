081acc523f9d2e3b1505fe11ccae216466c64e64
Mefodei <mefodei.link@gmail.com>
2020-10-05 00:55:45 +0300

    - remove nested addressables

--- UniGame.AddressableTools/Runtime/SpriteAtlases/Abstract/IAddressablesAtlasesLoader.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UniModules.UniGame.AddressableTools.Runtime.SpriteAtlases.Abstract
-{
-    using Cysharp.Threading.Tasks;
-
-    public interface IAddressablesAtlasesLoader
-    {
-        UniTask<bool> RequestSpriteAtlas(string guid);
-    }
-}
