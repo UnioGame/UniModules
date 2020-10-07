6cec2197005259fad5970e6754e0981abd886935
Mefodei <mefodei.link@gmail.com>
2020-10-07 20:25:14 +0300

    - update package

--- UniGame.Neuecc.Unirx/UniRx/Scripts/Observable.cs
+++ UniGame.Neuecc.Unirx/UniRx/Scripts/Observable.cs
@@ -289,18 +289,5 @@ namespace UniRx
             return new ForEachAsyncObservable<T>(source, onNext);
         }
 
-        public static IObservable<T> ToChain<T>(this IObservable<T> source, IObservable<Unit> skipSource = null)
-        {
-            return new ChainObservable<T>(source, skipSource);
-        }
-
-        public static IObservable<T> AddToChain<T>(this IObservable<T> source, IObservable<T> other)
-        {
-            if (source is ChainObservable<T> chainObservable) {
-                return chainObservable.Add(other);
-            }
-
-            return ToChain(source).AddToChain(other);
-        }
     }
 }
