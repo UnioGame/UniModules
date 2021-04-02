4f83f88cf2f1dd0583509f6e974da66d29aa29b3
Mefodei <mefodei.link@gmail.com>
2021-04-02 14:55:34 +0300

    - add remote data module

--- UniGame.RemoteData/Authorization/AbstractAuthTokenProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UniModules.UniGame.RemoteData.Authorization
-{
-    using System.Threading.Tasks;
-
-    public abstract class AbstractAuthTokenProvider<T> where T : class, IAuthToken
-    {
-        public abstract Task<T> FetchToken();
-    }
-}
