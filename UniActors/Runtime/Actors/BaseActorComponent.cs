0bfab214e71e826f433a7c467be8940b734d49fe
Mefodei <mefodei.link@gmail.com>
2021-06-22 02:53:02 +0300

    - pool update

--- UniActors/Runtime/Actors/BaseActorComponent.cs
+++ UniActors/Runtime/Actors/BaseActorComponent.cs
@@ -74,7 +74,7 @@
         {
             var actorTransform = transform;
             
-            var state = ObjectPool.Spawn<ActorBehaviour>(behaviourSource, 
+            var state = ObjectPoolAsset.Spawn<ActorBehaviour>(behaviourSource, 
                 Vector3.zero, Quaternion.identity,actorTransform, false);
             state.gameObject.SetActive(true);
             
