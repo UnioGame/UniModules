5a0580c70baf579f374a3d015ff7ab7d9fef5803
Mefodei <mefodei.link@gmail.com>
2023-05-24 01:17:26 +0400

    - move to view ids

--- UniGame.UnityFigma/Editor/FlowGenerator/ViewSystemFigmaFlowGenerator.cs
+++ UniGame.UnityFigma/Editor/FlowGenerator/ViewSystemFigmaFlowGenerator.cs
@@ -27,47 +27,52 @@ namespace UniGame.UnityFigma.Editor.FlowGenerator
             if (CheckAddButtonBehaviour(node, figmaImportProcessData))
             {
                 if (nodeGameObject.GetComponent<Button>() == null)
-                {
-                    var newButtonComponent = nodeGameObject.AddComponent<Button>();
-
-                    // Find target graphic if appropriate for showing selected state
-                    for (var i = 0; i < nodeGameObject.transform.childCount; i++)
-                    {
-                        var child = nodeGameObject.transform.GetChild(i);
-                        if (!child.name.ToLower().Contains("selected")) continue;
-                        
-                        newButtonComponent.targetGraphic = child.GetComponent<Graphic>();
-                        newButtonComponent.transition = Selectable.Transition.ColorTint;
-                        newButtonComponent.colors = new ColorBlock
-                        {
-                            disabledColor = new Color(0, 0, 0, 0),
-                            normalColor = new Color(0, 0, 0, 0),
-                            highlightedColor = Color.white,
-                            pressedColor = Color.white,
-                            selectedColor = Color.white,
-                            colorMultiplier = 1,
-                        };
-                    }
-                }
+                    AddButton(nodeGameObject);
             }
 
             if (!figmaImportProcessData.Settings.BuildPrototypeFlow) return;
             
             // Implement button if it has a prototype connection attached
             if (string.IsNullOrEmpty(node.transitionNodeID)) return;
-            
-            var prototypeFlowButton = nodeGameObject.AddComponent<FigmaPrototypeFlowButton>();
-            prototypeFlowButton.TargetScreenNodeId = node.transitionNodeID;
+
+            var targetNodeId = node.transitionNodeID;
+            var targetNode = figmaImportProcessData.NodeLookupDictionary[targetNodeId]; 
+            //targetNode.name
+            //var prototypeFlowButton = nodeGameObject.AddComponent<>();
+            //prototypeFlowButton.TargetScreenNodeId = node.transitionNodeID;
             // Future options to add transition information
         }
 
+        public void AddButton(GameObject nodeGameObject)
+        {
+            var newButtonComponent = nodeGameObject.AddComponent<Button>();
+
+            // Find target graphic if appropriate for showing selected state
+            for (var i = 0; i < nodeGameObject.transform.childCount; i++)
+            {
+                var child = nodeGameObject.transform.GetChild(i);
+                if (!child.name.ToLower().Contains("selected")) continue;
+                        
+                newButtonComponent.targetGraphic = child.GetComponent<Graphic>();
+                newButtonComponent.transition = Selectable.Transition.ColorTint;
+                newButtonComponent.colors = new ColorBlock
+                {
+                    disabledColor = new Color(0, 0, 0, 0),
+                    normalColor = new Color(0, 0, 0, 0),
+                    highlightedColor = Color.white,
+                    pressedColor = Color.white,
+                    selectedColor = Color.white,
+                    colorMultiplier = 1,
+                };
+            }
+        }
+        
         public override bool CheckAddButtonBehaviour(Node node, FigmaImportProcessData figmaImportProcessData)
         {
             // Apply rules
             if (node.name.ToLower().Contains("button")) return true;
-            if (figmaImportProcessData.Settings.BuildPrototypeFlow && !string.IsNullOrEmpty(node.transitionNodeID))
-                return true;
-            return false;
+            return figmaImportProcessData.Settings.BuildPrototypeFlow && 
+                   !string.IsNullOrEmpty(node.transitionNodeID);
         }
     }
 }
