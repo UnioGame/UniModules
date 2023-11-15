9ff612a4942f6b0306a35a95ae39952740209811
Mefodei <mefodei.link@gmail.com>
2023-11-15 22:35:41 +0700

    - remove json converter and move to packages

--- UniGame.Newtonsoft.Json-For-Unity/Newtonsoft.Json-for-Unity.Converters/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-
-using System.Runtime.CompilerServices;
-using UnityEngine.Scripting;
-
-[assembly: Preserve]
-[assembly: InternalsVisibleTo("Newtonsoft.Json.UnityConverters.Addressables")]
-[assembly: InternalsVisibleTo("Newtonsoft.Json.UnityConverters.Editor")]
-[assembly: InternalsVisibleTo("Newtonsoft.Json.UnityConverters.Tests")]
-[assembly: InternalsVisibleTo("Newtonsoft.Json.UnityConverters.Tests.Addressables")]
